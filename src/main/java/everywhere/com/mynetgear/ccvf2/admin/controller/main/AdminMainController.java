d05f9fe1d6dad80c7042ad281a2a759c1385f135
BAE SEONGUK <ccvf2@naver.com>
2015-12-07 21:49:16 +0900

    zzz

--- src/main/java/everywhere/com/mynetgear/ccvf2/admin/controller/main/AdminMainController.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/admin/controller/main/AdminMainController.java
@@ -1,16 +1,12 @@
 package everywhere.com.mynetgear.ccvf2.admin.controller.main;
 
-import java.io.File;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import everywhere.com.mynetgear.ccvf2.admin.service.main.AdminMainService;
