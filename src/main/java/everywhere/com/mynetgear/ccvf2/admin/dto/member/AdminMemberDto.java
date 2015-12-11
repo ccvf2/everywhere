655fb23fc77c11bc2699b66823780afcef79547f
SeongGook <ksg9155@gmail.com>
2015-12-11 11:55:58 +0900

    파일 생성

---
+++ src/main/java/everywhere/com/mynetgear/ccvf2/admin/dto/member/AdminMemberDto.java
@@ -0,0 +1,11 @@
+package everywhere.com.mynetgear.ccvf2.admin.dto.member;
+
+/**
+ * @author 곽성국
+ * @createDate 2015. 12. 11.
+ * @described 관리자 회원관리 DTO
+ * @reference class
+ */
+public class AdminMemberDto {
+
+}
