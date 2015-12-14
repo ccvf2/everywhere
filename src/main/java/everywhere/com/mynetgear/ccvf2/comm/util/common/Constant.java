a35ee17ed6f4bdf0f4d1e23f304193159d150082
SeongGook <ksg9155@gmail.com>
2015-12-14 17:24:10 +0900

    Edited Constandt accmpany status code

--- src/main/java/everywhere/com/mynetgear/ccvf2/comm/util/common/Constant.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/comm/util/common/Constant.java
@@ -98,11 +98,13 @@ public class Constant {
 	/** 핸드폰 인증 상태: 비활성 */
 	public static final String MEMBER_P_STATUS_LOCK = "M2002";
 	
-	/**여행동행구하기 게시글 종류: 구하는 중*/
-	public static final String ACCOMPANY_TYPE_OBTAINING = "H0001";
-	/**여행동행구하기 게시글 종류: 구함*/
-	public static final String ACCOMPANY_TYPE_COMPLETED = "H0002";
 	/**여행동행구하기 게시글 종류: 공지*/
-	public static final String ACCOMPANY_TYPE_NOTICE = "H0003";
+	public static final String ACCOMPANY_TYPE_NOTICE = "H0001";
+	/**여행동행구하기 게시글 종류: 한국 출발*/
+	public static final String ACCOMPANY_TYPE_FROMKOREA = "H0002";
+	/**여행동행구하기 게시글 종류: 여행중*/
+	public static final String ACCOMPANY_TYPE_ONTRIP = "H0003";
+	/**여행동행구하기 게시글 종류: 완료*/
+	public static final String ACCOMPANY_TYPE_COMPLETED = "H0004";
 	
 }
