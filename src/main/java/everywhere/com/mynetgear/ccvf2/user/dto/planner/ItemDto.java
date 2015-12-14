046a6b966b359a3b6150a73ee7818b48471dfbf1
heejin <heejin0103@gmail.com>
2015-12-14 11:37:34 +0900

    planner 읽기 수정

--- src/main/java/everywhere/com/mynetgear/ccvf2/user/dto/planner/ItemDto.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/dto/planner/ItemDto.java
@@ -3,6 +3,8 @@ package everywhere.com.mynetgear.ccvf2.user.dto.planner;
 import java.sql.Timestamp;
 import java.util.List;
 
+import everywhere.com.mynetgear.ccvf2.user.dto.spot.SpotDto;
+
 public class ItemDto {
 	private int item_no;
 	private int planner_no;
@@ -15,6 +17,7 @@ public class ItemDto {
 	private Timestamp start_time;
 	private Timestamp end_time;
 	
+	private SpotDto spot;
 	private List<MoneyDto> moneyList;
 	
 	public int getItem_no() {
@@ -23,65 +26,82 @@ public class ItemDto {
 	public void setItem_no(int item_no) {
 		this.item_no = item_no;
 	}
+	
 	public int getPlanner_no() {
 		return planner_no;
 	}
 	public void setPlanner_no(int planner_no) {
 		this.planner_no = planner_no;
 	}
+	
 	public int getMem_no() {
 		return mem_no;
 	}
 	public void setMem_no(int mem_no) {
 		this.mem_no = mem_no;
 	}
+	
 	public int getSpot_no() {
 		return spot_no;
 	}
 	public void setSpot_no(int spot_no) {
 		this.spot_no = spot_no;
 	}
+	
 	public int getItem_order() {
 		return item_order;
 	}
 	public void setItem_order(int item_order) {
 		this.item_order = item_order;
 	}
+	
 	public String getNote() {
 		return note;
 	}
 	public void setNote(String note) {
 		this.note = note;
 	}
+	
 	public String getAttach_photoes() {
 		return attach_photoes;
 	}
 	public void setAttach_photoes(String attach_photoes) {
 		this.attach_photoes = attach_photoes;
 	}
+	
 	public double getItem_star_score() {
 		return item_star_score;
 	}
 	public void setItem_star_score(double item_star_score) {
 		this.item_star_score = item_star_score;
 	}
+	
 	public Timestamp getStart_time() {
 		return start_time;
 	}
 	public void setStart_time(Timestamp start_time) {
 		this.start_time = start_time;
 	}
+	
 	public Timestamp getEnd_time() {
 		return end_time;
 	}
 	public void setEnd_time(Timestamp end_time) {
 		this.end_time = end_time;
-	}	
+	}
+	
 	public List<MoneyDto> getMoneyList() {
 		return moneyList;
 	}
 	public void setMoneyList(List<MoneyDto> moneyList) {
 		this.moneyList = moneyList;
+	}
+	
+	public SpotDto getSpot() {
+		return spot;
+	}
+	public void setSpot(SpotDto spot) {
+		this.spot = spot;
 	}
 	
 	@Override
