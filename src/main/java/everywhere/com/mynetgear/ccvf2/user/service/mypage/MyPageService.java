1cdb7b0cf85c15eb1ad3afa39e189306e8c5b385
김성광 <tjdrhkd88@nate.com>
2015-12-15 20:10:17 +0900

    타회원 페이지로 이동

--- src/main/java/everywhere/com/mynetgear/ccvf2/user/service/mypage/MyPageService.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/service/mypage/MyPageService.java
@@ -1,5 +1,9 @@
 package everywhere.com.mynetgear.ccvf2.user.service.mypage;
 
+import org.springframework.web.servlet.ModelAndView;
+
 public interface MyPageService {
+
+	public ModelAndView moveUserPage(ModelAndView mav);
 
 }
--- src/main/java/everywhere/com/mynetgear/ccvf2/user/service/mypage/MyPageServiceImpl.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/service/mypage/MyPageServiceImpl.java
@@ -1,12 +1,31 @@
 package everywhere.com.mynetgear.ccvf2.user.service.mypage;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
-import everywhere.com.mynetgear.ccvf2.user.dao.mypage.MyPageDao;
+import everywhere.com.mynetgear.ccvf2.user.dao.member.MemberDao;
+import everywhere.com.mynetgear.ccvf2.user.dto.member.MemberDto;
 
 @Component
 public class MyPageServiceImpl implements MyPageService {
 	@Autowired
-	private MyPageDao myPageDao;
+	private MemberDao memberDao;
+
+	@Override
+	public ModelAndView moveUserPage(ModelAndView mav) {
+		Map<String, Object> map = mav.getModelMap();
+		HttpServletRequest request= (HttpServletRequest)map.get("request");
+		int mem_no=Integer.parseInt(request.getParameter("mem_no"));
+		MemberDto dto = memberDao.memberRead(mem_no);
+		
+		mav.addObject("dto", dto);
+		mav.setViewName("/user/myPage/myPage");
+		
+		return mav;
+	}
 }
