f99b1f61eb9ee0688e6ccd0df413b7a4ffb656f0
SeongGook <ksg9155@gmail.com>
2015-12-17 21:10:57 +0900

    회원관리 페이지 자바스크립트 분리

---
+++ src/main/webapp/script/admin/member/changeMemberStatus.js
@@ -0,0 +1,22 @@
+/**
+ * 관리자 회원정보 관리에서 회원 상태를 변경하는 js
+ */
+// 회원 상태 select 박스가 변경되었을때 호출됨
+function changeStatus(mem_no) {
+	var selected_status=$("#memStatus"+mem_no+" option:selected").val();
+	/* alert(selected); */
+	$.ajax({
+		url: "/admin/member/adminMemberUpdate.do?mem_no="+mem_no+"&mem_status_code="+selected_status,
+		type:"get",
+		dataType:"text",
+		success: changeStatusSuccess,
+		error:function(xhr, status, errorMsg){
+			alert(xhr+","+status+","+errorMsg);
+		}
+	});
+}
+//변경 성공시 selected로 변경하는 함수
+function changeStatusSuccess(check) {
+	//alert("회원상태가 수정되었습니다.");
+	everywhereAlert("상태 변경 메세지","회원상태가 수정되었습니다.");
+}
---
+++ src/main/webapp/script/admin/member/searchMember.js
@@ -0,0 +1,56 @@
+/**
+ * 관리자 회원관리에서 회원을 검색하는 js
+ */
+// 검색 버튼이 눌렷을때 호출됨
+function searchFun() {
+	var search=document.getElementById("search").value;
+	var memLevel=document.getElementById("memLevel").value;
+	var memStatus=document.getElementById("memStatus").value;
+	var phoneStatus=document.getElementById("phoneStatus").value;
+	var start_date=document.getElementById("start_date").value;
+	var end_date=document.getElementById("end_date").value;
+	
+	var searchOption=document.getElementById("searchOption");
+    var selectedOption = searchOption.options[searchOption.selectedIndex].value
+    /* 
+	alert("memLevel:" + memLevel);
+	alert("memStatus:" + memStatus);
+	alert("phoneStatus:" + phoneStatus);
+	alert("start_date:" + start_date);
+	alert("end_date:" + end_date);
+	alert("selectedOption: " + selectedOption); */
+	var url = "/admin/member/adminMemberList.do?";
+	
+	if(searchOption!="") {
+		if(selectedOption=="name") {
+			url+="name="+search;
+		} else if(selectedOption=="email") {
+			url+="email="+search;
+		} else if(selectedOption=="emailname"){
+			url+="name="+search+"&email="+search;
+		}
+	}
+	
+	
+	if(memLevel!="") {
+		url+="&memLevel="+memLevel;
+	}
+	
+	if(memStatus!=""){
+		url+="&memStatus="+memStatus;
+	}
+	
+	if(phoneStatus!=""){
+		url+="&phoneStatus="+phoneStatus;
+	}
+	
+	if(start_date!=""){
+		url+="&start_date="+start_date;
+	}
+	
+	if(end_date!=""){
+		url+="&end_date="+end_date;
+	}
+			
+	location.href=url;
+}
