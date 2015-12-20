5474b695b5849054e34d66f93c5bb6831b0cf73b
BAE SEONGUK <ccvf2@naver.com>
2015-12-21 01:48:54 +0900

    오픈api가져오기 & 가져온 사진이미지

--- src/main/java/everywhere/com/mynetgear/ccvf2/user/controller/main/UserMainController.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/controller/main/UserMainController.java
@@ -1,5 +1,7 @@
 package everywhere.com.mynetgear.ccvf2.user.controller.main;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import everywhere.com.mynetgear.ccvf2.admin.dto.settingmain.SettingMainDto;
+import everywhere.com.mynetgear.ccvf2.user.dto.planner.PlannerDto;
 import everywhere.com.mynetgear.ccvf2.user.service.main.UserMainService;
+import everywhere.com.mynetgear.ccvf2.user.service.planner.PlannerService;
 
 /**
  * @author 배성욱
@@ -22,12 +26,22 @@ import everywhere.com.mynetgear.ccvf2.user.service.main.UserMainService;
 public class UserMainController {
 	@Autowired
 	private UserMainService userMainService;
-
+	@Autowired
+	private PlannerService plannerService;
+	
+	
 	@RequestMapping(value="/user/main/main.do", method=RequestMethod.GET)
 	public ModelAndView userMainPage(HttpServletRequest request, HttpServletResponse response) {
 		ModelAndView mav = new ModelAndView("/user/main/userMain2");
 		SettingMainDto settingMainDto=userMainService.getOneBackGroundImg();
 		//메인페이지 띄우기
+		List<PlannerDto> plannerList=plannerService.getPlannerList(인자값);
+		
+		
+		
+		
+		
+		
 		mav.addObject("backgroungImg",settingMainDto.getSetting_url());
 		return mav;
 	}
