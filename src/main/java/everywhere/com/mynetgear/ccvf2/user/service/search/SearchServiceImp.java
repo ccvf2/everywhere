7e6fa91564e40a7b0ad68522847f01f868cb8380
SeongGook <ksg9155@gmail.com>
2015-12-28 11:59:31 +0900

    존재하지 않는 이미지에 대해서 대체 이미지가 나오도록 수정 AJAX 대신에 페이징처리가 되도록 수정

--- src/main/java/everywhere/com/mynetgear/ccvf2/user/service/search/SearchServiceImp.java
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/service/search/SearchServiceImp.java
@@ -72,6 +72,8 @@ public class SearchServiceImp implements SearchService {
 		
 		spotDto.setSearchWord1(searchPlace);
 		spotDto.setSearchWord2(searchSpot);
+		
+		int count = searchDao.getSpotCount(spotDto);
 		List<SpotDto> searchSpotList = searchDao.getSpotList(spotDto);
 		EverywhereAspect.logger.info(EverywhereAspect.logMsg + searchSpotList.size());
 		for(int i = 0; i < searchSpotList.size(); i++) {
@@ -95,6 +97,9 @@ public class SearchServiceImp implements SearchService {
 		
 		List<CommonCodeDto> spotTypeList = commonCodeService.getListCodeGroup("T0001");
 		
+		mav.addObject("count", count);
+		mav.addObject("currentPage", currentPage);
+		mav.addObject("boardSize", spotSize);
 		mav.addObject("searchSpot", searchSpot);
 		mav.addObject("searchPlace", searchPlace);
 		mav.addObject("searchSpotList", searchSpotList);
@@ -102,161 +107,7 @@ public class SearchServiceImp implements SearchService {
 		mav.addObject("spotTypeList", spotTypeList);
 		mav.setViewName("user/search/searchSpot");
 	}
-
-
-	@Override
-	public void getSpotList(ModelAndView mav) {
-		Map<String, Object> map = mav.getModelMap();
-		HttpServletRequest request = (HttpServletRequest) map.get("request");
-		HttpServletResponse response = (HttpServletResponse) map.get("response");
-		
-		//지역 검색값
-		String searchPlace = request.getParameter("searchPlace");
-		//명소명 검색값
-		String searchSpot = request.getParameter("searchSpot");
-		
-		//현재 페이지
-		String seach_page = request.getParameter("seach_page");
-		//넘어온 페이지 값이 없으면 1로
-		if(seach_page == null)
-			seach_page = "1";
-		int currentPage = Integer.parseInt(seach_page);
-		
-		
-		SpotDtoExt spotDto = new SpotDtoExt();
-		spotDto.setSearchWord1(searchPlace);
-		spotDto.setSearchWord2(searchSpot);
-		spotDto.setCurrentPage(currentPage);
-		
-		List<SpotDto> searchSpotList = searchDao.getSpotList(spotDto);
-		EverywhereAspect.logger.info(EverywhereAspect.logMsg + searchSpotList.size());
-		//사진을 가져오는 부분
-		for(int i = 0; i < searchSpotList.size(); i++) {
-			if(searchSpotList.get(i).getAttach_file() != null) {
-				String[] attach_no = searchSpotList.get(i).getAttach_file().split(",");
-				List<CommonFileIODto> fileList = new ArrayList<CommonFileIODto>();
-				CommonFileIODto fileIODto = commonFileIODao.getOneFileDto(Integer.parseInt(attach_no[0]));
-				fileList.add(fileIODto);
-				System.out.println(fileIODto);
-				searchSpotList.get(i).setSpot_photoes(fileList);
-			}
-		}
-		
-		JSONArray jsonArray = new JSONArray();
-		JSONObject rootObj = new JSONObject();
-		for (int i = 0; i < searchSpotList.size(); i++) {
-			SpotDto dto = searchSpotList.get(i);
-			JSONObject obj = new JSONObject();
-			obj.put("spot_no", dto.getSpot_no());
-			obj.put("mem_no", dto.getMem_no());
-			obj.put("country_code", StringUtils.clean(dto.getCountry_code()));
-			obj.put("city_code", StringUtils.clean(dto.getCity_code()));
-			obj.put("spot_name", StringUtils.clean(dto.getSpot_name()));
-			obj.put("spot_type_code", StringUtils.clean(dto.getSpot_type_code()));
-			obj.put("mem_level_code", StringUtils.clean(dto.getMem_level_code()));
-			obj.put("spot_note", StringUtils.clean(dto.getSpot_note()));
-			obj.put("spot_addr", StringUtils.clean(dto.getSpot_addr()));
-			obj.put("spot_lat", dto.getSpot_lat());
-			obj.put("spot_long", dto.getSpot_long());
-			obj.put("total_star_score", dto.getTotal_star_score());
-			if(dto.getSpot_photoes() != null){
-				obj.put("spot_photo_save_name", StringUtils.clean(dto.getSpot_photoes().get(0).getSave_name()));
-				obj.put("spot_photo_extension", StringUtils.clean(dto.getSpot_photoes().get(0).getExtension()));
-			}else
-			{
-				obj.put("spot_photo_save_name", StringUtils.clean("No_Image"));
-				obj.put("spot_photo_extension", StringUtils.clean("png"));
-			}
-			jsonArray.add(obj);
-		}
-		
-		try{
-			rootObj.put("spot", jsonArray);
-			String json = rootObj.toJSONString();
-			System.out.println(json);
-			response.setContentType("application/html;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.print(json);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
-
-	@Override
-	public void getMoreSpotList(ModelAndView mav) {
-		Map<String, Object> map=mav.getModelMap();
-		HttpServletRequest request = (HttpServletRequest)map.get("request");
-		HttpServletResponse response = (HttpServletResponse)map.get("response");
-		
-		//나라 및 지역 입력값
-		String searchPlace = request.getParameter("searchPlace");
-		//명소명 입력값
-		String searchSpot = request.getParameter("searchSpot");
-
-		String seach_page = request.getParameter("search_page");
-		if(seach_page == null)
-			seach_page = "1";
-		//페이지 처리
-		int currentPage = Integer.parseInt(seach_page);
-		
-		SpotDtoExt spotDto = new SpotDtoExt();
-		spotDto.setSearchWord1(searchPlace);
-		spotDto.setSearchWord2(searchSpot);
-		spotDto.setCurrentPage(currentPage);
-		
-		List<SpotDto> searchSpotList = searchDao.getSpotList(spotDto);
-		for(int i = 0; i < searchSpotList.size(); i++){
-			if(searchSpotList.get(i).getAttach_file() != null){
-				String[] attach_no = searchSpotList.get(i).getAttach_file().split(",");
-				List<CommonFileIODto> fileList = new ArrayList<CommonFileIODto>();
-				CommonFileIODto fileIODto = commonFileIODao.getOneFileDto(Integer.parseInt(attach_no[0]));
-				fileList.add(fileIODto);
-				System.out.println(fileIODto);
-				searchSpotList.get(i).setSpot_photoes(fileList);
-			}
-		}
-
-		JSONArray jsonArray = new JSONArray();
-		JSONObject rootObj = new JSONObject();
-		for (int i = 0; i < searchSpotList.size(); i++) {
-			SpotDto dto = searchSpotList.get(i);
-			JSONObject obj = new JSONObject();
-			obj.put("spot_no", dto.getSpot_no());
-			obj.put("mem_no", dto.getMem_no());
-			obj.put("country_code", StringUtils.clean(dto.getCountry_code()));
-			obj.put("city_code", StringUtils.clean(dto.getCity_code()));
-			obj.put("spot_name", StringUtils.clean(dto.getSpot_name()));
-			obj.put("spot_type_code", StringUtils.clean(dto.getSpot_type_code()));
-			obj.put("mem_level_code", StringUtils.clean(dto.getMem_level_code()));
-			obj.put("spot_note", StringUtils.clean(dto.getSpot_note()));
-			obj.put("spot_addr", StringUtils.clean(dto.getSpot_addr()));
-			obj.put("spot_lat", dto.getSpot_lat());
-			obj.put("spot_long", dto.getSpot_long());
-			obj.put("total_star_score", dto.getTotal_star_score());
-			if(dto.getSpot_photoes() != null){
-				obj.put("spot_photo_save_name", StringUtils.clean(dto.getSpot_photoes().get(0).getSave_name()));
-				obj.put("spot_photo_extension", StringUtils.clean(dto.getSpot_photoes().get(0).getExtension()));
-			}else
-			{
-				obj.put("spot_photo_save_name", StringUtils.clean("No_Image"));
-				obj.put("spot_photo_extension", StringUtils.clean("png"));
-			}
-			jsonArray.add(obj);
-		}
-		
-		try{
-			rootObj.put("spot", jsonArray);
-			String json = rootObj.toJSONString();
-			System.out.println(json);
-			response.setContentType("application/html;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.print(json);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	@Override
 	public void searchPlanner(ModelAndView mav) {
 		Map<String, Object> map = mav.getModelMap();
