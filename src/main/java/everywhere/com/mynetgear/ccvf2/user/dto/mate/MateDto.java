057b14523152c85a1cdf7374206952bcd39862bd
김성광 <tjdrhkd88@nate.com>
2016-01-21 08:05:34 +0900

    mateDto

---
+++ src/main/java/everywhere/com/mynetgear/ccvf2/user/dto/mate/MateDto.java
@@ -0,0 +1,25 @@
+package everywhere.com.mynetgear.ccvf2.user.dto.mate;
+
+/**
+ * @author 김성광
+ * @createDate 2015. 12. 25.
+ * @described 친구관계
+ * @reference class
+ */
+public class MateDto {
+	private int mem_no;
+	private int mate_no;
+	
+	public int getMem_no() {
+		return mem_no;
+	}
+	public void setMem_no(int mem_no) {
+		this.mem_no = mem_no;
+	}
+	public int getMate_no() {
+		return mate_no;
+	}
+	public void setMate_no(int mate_no) {
+		this.mate_no = mate_no;
+	}
+}
